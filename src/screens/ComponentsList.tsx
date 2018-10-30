02fd3ac9d8fc752f2581ecab51d072417822533a
Divyansh Mahajan <divyanshmahajan.24@gmail.com>
2018-10-30 16:18:35 +0530

    feat(component-list): add component list screen

--- src/screens/ComponentsList.tsx
+++ src/screens/ComponentsList.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View, ViewStyle } from 'react-native';
+
+import Ripple from '../components/ripple';
 
 class ComponentsList extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Button</Text>
+      <View style={styles.wrapper}>
+        <Ripple
+          style={styles.listItem}
+          // onPress={() => this.props.navigation.navigate('Details')}
+        >
+          <Text>Buttons</Text>
+        </Ripple>
+        <Ripple style={styles.listItem}>
+          <Text>Ripple</Text>
+        </Ripple>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  listItem: {
+    paddingVertical: 16,
+    paddingHorizontal: 12,
+    borderStyle: 'solid',
+    borderColor: '#ddd',
+    borderWidth: 1,
+  } as ViewStyle,
+});
 
 export default ComponentsList;
